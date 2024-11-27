0fa7c98984e7b3e805f2538fe8e9fd6d1fb887ca
Rahul <rahulracharla369@gmail.com>
2024-11-28 00:09:59 +0530

    method overriding assignment completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,11 +1,5 @@
 package com.driver;
 
-class A {
-  public String meth() {
-    return  "Invoking method from class A";
-  }
-}
-
 class B extends A{
   @Override
   public String meth() {
--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -3,6 +3,21 @@ package com.driver.test;
 import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
 
+class A {
+    public String meth() {
+        return  "Invoking method from class A";
+    }
+}
+
+class B extends com.driver.A {
+    @Override
+    public String meth() {
+        return "Method is overridden in Extended class B";
+    }
+}
 public class TestCases {
-    
+    public static void main(String[] args) {
+        B objB = new B();
+        System.out.println(objB.meth());
+    }
 }
