fe927a97eda625fde4ccad4489cbade517e77371
rahulracharlacoder <rahulracharla369@gmail.com>
2024-12-02 18:27:09 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public String meth() {return "Method is overridden in Extendend class B";}
 }
   public static void main(String[] args) {
-    B b = new B();
+    A b = new B();
     System.out.println(b.meth());
   }
 }
